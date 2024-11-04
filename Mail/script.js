27098f40c30458ffba2c3d51a5a0b3b4dd288db6
TommasoVilla91 <tommasovilla@yahoo.it>
2024-11-04 17:01:47 +0100

    feat: mail exercise completed

--- Mail/script.js
+++ Mail/script.js
@@ -1,18 +1,26 @@
+// DICHIARAZIONI DI VARIABILI
 // User e-mail
 const userEmail = prompt("Inserisci qui la tua e-mail");
 
 // Lista e-mail 
-const emailList = [
-    ["mimmo@gmail.com", "arnaldo@yahoo.it", "paolona@gmail.com", "ziobilly@libero.it"],
-    ["aurora@msn.it", "jhonny@gmail.com","aiace@facebook.it"]
-];
+const emailList = ["mimmo@gmail.com", "arnaldo@yahoo.it", "paolona@gmail.com", "ziobilly@libero.it"];
 
-// Fa si che venga analizzata la mail inserita dall'utente per constatare che ci sia almeno in una delle due liste
+// funzione
+let isFound = false;
 
-for (let i = 0; i < emailList.length; i++) {
-    const curList = emailList[i];
-        for (let j = 0; j < curList.length; j++) {
-            curList[j] === userEmail;
+// LOGICA
+// Fa si che venga analizzata la mail inserita dall'utente per constatare che ci sia o meno nella lista
 
-        }
+for (let i = 0; i <= emailList.length; i++) {
+    const curEmail = emailList[i];
+    if (userEmail === curEmail[i]) {
+        isFound = true;
+    }
+}
+
+// OUTPUT
+if (isFound === true) {
+    console.log(`La tua email ${userEmail} è valida`);
+} else {
+    console.log(`La tua email non è valda`);
 }
